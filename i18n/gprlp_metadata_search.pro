a493249c356fd0471a11f9ba4f0335e3485b20f6
Armin Retterath <armin.retterath@googlemail.com>
2022-03-04 15:22:18 +0100

    Add files via upload
    German translation

---
+++ i18n/gprlp_metadata_search.pro
@@ -0,0 +1,3 @@
+FORMS = ../gprlp_metadata_search_dialog_base.ui
+SOURCES = ../gprlp_metadata_search_dialog.py ../gprlp_metadata_search.py
+TRANSLATIONS = gprlp_metadata_search_de.ts
---
+++ i18n/gprlp_metadata_search_de.ts
@@ -0,0 +1,270 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE" sourcelanguage="en_GB">
+<context>
+    <name>GeoportalRlpMetadataSearch</name>
+    <message>
+        <location filename="../gprlp_metadata_search.py" line="241"/>
+        <source>&amp;GeoPortal.rlp Metadata Search</source>
+        <translation>&amp;GeoPortal.rlp Metadatensuche</translation>
+    </message>
+    <message>
+        <location filename="../gprlp_metadata_search.py" line="228"/>
+        <source>GeoPortal.rlp metadata search</source>
+        <translation>GeoPortal.rlp Metadatensuche</translation>
+    </message>
+    <message>
+        <location filename="../gprlp_metadata_search.py" line="343"/>
+        <source>Resource registration</source>
+        <translation>Datenquellen Registrierung</translation>
+    </message>
+    <message>
+        <location filename="../gprlp_metadata_search.py" line="452"/>
+        <source>OWS Context</source>
+        <translation>OWS Context</translation>
+    </message>
+    <message>
+        <location filename="../gprlp_metadata_search.py" line="102"/>
+        <source>Map Layer</source>
+        <translation>Kartenebene</translation>
+    </message>
+    <message>
+        <location filename="../gprlp_metadata_search.py" line="894"/>
+        <source>Dataset</source>
+        <translation>Datensatz</translation>
+    </message>
+    <message>
+        <location filename="../gprlp_metadata_search.py" line="110"/>
+        <source>Remote CSW</source>
+        <translation>Entfernter CSW</translation>
+    </message>
+    <message>
+        <location filename="../gprlp_metadata_search.py" line="118"/>
+        <source>Rhineland-Palatinate</source>
+        <translation>Rheinland-Pfalz</translation>
+    </message>
+    <message>
+        <location filename="../gprlp_metadata_search.py" line="122"/>
+        <source>Hesse</source>
+        <translation>Hessen</translation>
+    </message>
+    <message>
+        <location filename="../gprlp_metadata_search.py" line="126"/>
+        <source>Saarland</source>
+        <translation>Saarland</translation>
+    </message>
+    <message>
+        <location filename="../gprlp_metadata_search.py" line="384"/>
+        <source>Context layer</source>
+        <translation>Context Ebene</translation>
+    </message>
+    <message>
+        <location filename="../gprlp_metadata_search.py" line="1354"/>
+        <source>No preview</source>
+        <translation>Keine Vorschau</translation>
+    </message>
+    <message>
+        <location filename="../gprlp_metadata_search.py" line="523"/>
+        <source>Map layer</source>
+        <translation>Kartenebene</translation>
+    </message>
+    <message>
+        <location filename="../gprlp_metadata_search.py" line="1373"/>
+        <source>Open in Browser</source>
+        <translation>In Browser öffnen</translation>
+    </message>
+    <message>
+        <location filename="../gprlp_metadata_search.py" line="1449"/>
+        <source>Online Metadata</source>
+        <translation>Online Metadaten</translation>
+    </message>
+    <message>
+        <location filename="../gprlp_metadata_search.py" line="879"/>
+        <source>Searching...</source>
+        <translation>Suche...</translation>
+    </message>
+    <message>
+        <location filename="../gprlp_metadata_search.py" line="887"/>
+        <source>Ready</source>
+        <translation>Fertig</translation>
+    </message>
+    <message>
+        <location filename="../gprlp_metadata_search.py" line="1154"/>
+        <source>Access options</source>
+        <translation>Zugriffsmöglichkeiten</translation>
+    </message>
+    <message>
+        <location filename="../gprlp_metadata_search.py" line="1200"/>
+        <source>Download options</source>
+        <translation>Downloadoptionen</translation>
+    </message>
+    <message>
+        <location filename="../gprlp_metadata_search.py" line="1201"/>
+        <source>View options</source>
+        <translation>Anzeigeoptionen</translation>
+    </message>
+    <message>
+        <location filename="../gprlp_metadata_search.py" line="1293"/>
+        <source>WMS Layer</source>
+        <translation>WMS Ebene</translation>
+    </message>
+    <message>
+        <location filename="../gprlp_metadata_search.py" line="1311"/>
+        <source>No organisation found</source>
+        <translation>Keine Organisation gefunden</translation>
+    </message>
+    <message>
+        <location filename="../gprlp_metadata_search.py" line="1645"/>
+        <source>Preview...</source>
+        <translation>Vorschau...</translation>
+    </message>
+    <message>
+        <location filename="../gprlp_metadata_search.py" line="1750"/>
+        <source>Found OWS Context documents</source>
+        <translation>Gefundene Context Dokumente</translation>
+    </message>
+    <message>
+        <location filename="../gprlp_metadata_search.py" line="1752"/>
+        <source>Found map layers (WMS)</source>
+        <translation>Gefundene WMS Ebenen</translation>
+    </message>
+    <message>
+        <location filename="../gprlp_metadata_search.py" line="1754"/>
+        <source>Found datasets</source>
+        <translation>Gefundene Datensätze</translation>
+    </message>
+</context>
+<context>
+    <name>GeoportalRlpMetadataSearchDialogBase</name>
+    <message>
+        <location filename="../gprlp_metadata_search_dialog_base.ui" line="14"/>
+        <source>GeoPortal.rlp Metadata Search</source>
+        <translation>GeoPortal.rlp Metadatensuche</translation>
+    </message>
+    <message>
+        <location filename="../gprlp_metadata_search_dialog_base.ui" line="58"/>
+        <source>Start Search</source>
+        <translation>Suchen</translation>
+    </message>
+    <message>
+        <location filename="../gprlp_metadata_search_dialog_base.ui" line="71"/>
+        <source>SearchText</source>
+        <translation>Suchtext</translation>
+    </message>
+    <message>
+        <location filename="../gprlp_metadata_search_dialog_base.ui" line="84"/>
+        <source>Results</source>
+        <translation>Ergebnisse</translation>
+    </message>
+    <message>
+        <location filename="../gprlp_metadata_search_dialog_base.ui" line="107"/>
+        <source>Preview</source>
+        <translation>Vorschau</translation>
+    </message>
+    <message>
+        <location filename="../gprlp_metadata_search_dialog_base.ui" line="123"/>
+        <source>Load</source>
+        <translation>Laden</translation>
+    </message>
+    <message>
+        <location filename="../gprlp_metadata_search_dialog_base.ui" line="617"/>
+        <source>Unknown</source>
+        <translation>Unbekannt</translation>
+    </message>
+    <message>
+        <location filename="../gprlp_metadata_search_dialog_base.ui" line="173"/>
+        <source>Page</source>
+        <translation>Seite</translation>
+    </message>
+    <message>
+        <location filename="../gprlp_metadata_search_dialog_base.ui" line="225"/>
+        <source>0</source>
+        <translation>0</translation>
+    </message>
+    <message>
+        <location filename="../gprlp_metadata_search_dialog_base.ui" line="212"/>
+        <source>from</source>
+        <translation>von</translation>
+    </message>
+    <message>
+        <location filename="../gprlp_metadata_search_dialog_base.ui" line="238"/>
+        <source>&lt;</source>
+        <translation>&lt;</translation>
+    </message>
+    <message>
+        <location filename="../gprlp_metadata_search_dialog_base.ui" line="251"/>
+        <source>&gt;</source>
+        <translation>&gt;</translation>
+    </message>
+    <message>
+        <location filename="../gprlp_metadata_search_dialog_base.ui" line="295"/>
+        <source>Further information</source>
+        <translation>Weitere Infos</translation>
+    </message>
+    <message>
+        <location filename="../gprlp_metadata_search_dialog_base.ui" line="318"/>
+        <source>Logo</source>
+        <translation>Logo</translation>
+    </message>
+    <message>
+        <location filename="../gprlp_metadata_search_dialog_base.ui" line="331"/>
+        <source>Resource type:</source>
+        <translation>Typ:</translation>
+    </message>
+    <message>
+        <location filename="../gprlp_metadata_search_dialog_base.ui" line="357"/>
+        <source>Resource id:</source>
+        <translation>ID:</translation>
+    </message>
+    <message>
+        <location filename="../gprlp_metadata_search_dialog_base.ui" line="370"/>
+        <source>Load count:</source>
+        <translation>Nutzung:</translation>
+    </message>
+    <message>
+        <location filename="../gprlp_metadata_search_dialog_base.ui" line="409"/>
+        <source>Organization:</source>
+        <translation>Organisation:</translation>
+    </message>
+    <message>
+        <location filename="../gprlp_metadata_search_dialog_base.ui" line="422"/>
+        <source>Last modified:</source>
+        <translation>Geändert:</translation>
+    </message>
+    <message>
+        <location filename="../gprlp_metadata_search_dialog_base.ui" line="435"/>
+        <source>Licence:</source>
+        <translation>Lizenz:</translation>
+    </message>
+    <message>
+        <location filename="../gprlp_metadata_search_dialog_base.ui" line="448"/>
+        <source>Restrictions:</source>
+        <translation>Restriktionen:</translation>
+    </message>
+    <message>
+        <location filename="../gprlp_metadata_search_dialog_base.ui" line="513"/>
+        <source>Access url:</source>
+        <translation>Zugriffspunkt:</translation>
+    </message>
+    <message>
+        <location filename="../gprlp_metadata_search_dialog_base.ui" line="539"/>
+        <source>Metadata:</source>
+        <translation>Metadaten:</translation>
+    </message>
+    <message>
+        <location filename="../gprlp_metadata_search_dialog_base.ui" line="565"/>
+        <source>Extent (EPSG:4326):</source>
+        <translation type="obsolete">Ausdehnung (EPSG:4326):</translation>
+    </message>
+    <message>
+        <location filename="../gprlp_metadata_search_dialog_base.ui" line="604"/>
+        <source>Online Usage:</source>
+        <translation>Online Nutzung:</translation>
+    </message>
+    <message>
+        <location filename="../gprlp_metadata_search_dialog_base.ui" line="565"/>
+        <source>Extent:</source>
+        <translation>Ausdehnung:</translation>
+    </message>
+</context>
+</TS>
